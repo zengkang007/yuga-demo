635557a4e4a1cb1c3f5702f604f19d7654a795d7
zengkang007@163.com <x86power>
2017-05-06 16:28:35 +0800

    initialization project.

---
+++ src/main/java/StartupApp.java
@@ -0,0 +1,6 @@
+/**
+ * Created by zengk on 2017/5/6.
+ * Added @ 2017-05-06
+ */
+public class StartupApp {
+}
---
+++ src/test/java/TestStartupApp.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zengk on 2017/5/6.
+ */
+public class TestStartupApp {
+}
